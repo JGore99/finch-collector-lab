3a24ab1806f5f5b692c2db9eae798a7b3542fd5b
Jeff_Gore <JefGore@gmail.com>
2021-12-21 20:20:24 -0500

    lectures 1 - 3 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Car(models.Model):
@@ -6,3 +7,10 @@ class Car(models.Model):
   model = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   year = models.IntegerField()
+
+  def __str__(self):
+    return self.make
+
+  def get_absolute_url(self):
+      return reverse("cars_detail", kwargs={"car_id": self.id})
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Car
-
-def cars_index(request):
-  cars = Car.objects.all()
-  return render(request, 'cars/index.html', { 'cars': cars})
-
-# Create your views here.
 from django.http import HttpResponse
+
 
 def home(request):
   return render(request, 'home.html')
@@ -14,6 +10,24 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+def cars_index(request):
+  cars = Car.objects.all()
+  return render(request, 'cars/index.html', { 'cars': cars})
+
 def cars_detail(request, car_id):
-  car = Car.ojects.get(id=car_id)
+  car = Car.objects.get(id=car_id)
   return render(request, 'cars/detail.html', { 'car': car })
+
+class CarCreate(CreateView):
+  model = Car
+  fields = ['make', 'model', 'description', 'year']
+  # success_url = '/cars/'
+
+class CarUpdate(UpdateView):
+  model = Car
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['model', 'description', 'year']
+
+class CarDelete(DeleteView):
+  model = Car
+  success_url = '/cars/'
